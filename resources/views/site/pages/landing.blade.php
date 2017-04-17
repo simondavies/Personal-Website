618ca9cfb4e739515ca00ec25985728f31b9d73a
Simon Davies <simondavies@live.co.uk>
2017-04-17 21:26:06 +0100

    re-wording

--- resources/views/site/pages/landing.blade.php
+++ resources/views/site/pages/landing.blade.php
@@ -5,7 +5,7 @@
             <div class="site-title-wrapper">
                 <h1>Simon Davies</h1>
                 <h2>Web Developer/Designer</h2>
-                <div class="construction-msg">Site creativity is currently been refurbished...</div>
+                <div class="construction-msg">Site creativity, is currently being refurbished...</div>
             </div>
             @include('site.partials.social-icons',['class' => 'landing'])
         </div><!-- eo:landing-content -->
